d411a1e99e5593b12fa9ead319a94f70bd0cda77
SvenWritesCode <kyletrusler@gmail.com>
2020-09-17 18:18:23 +0000

    someone should not have let me change settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
     sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/bundle.js',
+    format: "iife",
+    name: "app",
+    file: "public/bundle.js",
   },
   plugins: [
     svelte({
       // enable run-time checks when not in production
-      dev: true,
+      dev: !production,
       // we'll extract any component CSS out into
       // a separate file — better for performance
-      css: css => {
-        css.write('public/bundle.css');
+      css: (css) => {
+        css.write("public/bundle.css");
       },
     }),
 
@@ -35,7 +35,7 @@ export default {
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    livereload('public'),
+    !production && livereload("public"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
